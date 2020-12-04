d57c5c7511fe7db3283e8aaf94e25771969ec1fe
Balieiro <balieiro1396@gmail.com>
2020-12-04 14:58:31 -0300

    finalizado a parte de fatoração QR

--- practice/gram.py
+++ practice/gram.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy import linalg as LA
 
 def gramsch(matrix):
     arr = np.transpose(matrix)
@@ -12,7 +11,7 @@ def gramsch(matrix):
             r[k,j] = np.inner(q[k],arr[j])
             q[j] = q[j] - (r[k,j] * q[k])
 
-        r[j,j] = LA.norm(q[j])
+        r[j,j] = np.linalg.norm(q[j])
         if (r[j,j] == 0):
             continue
         else:
@@ -21,15 +20,50 @@ def gramsch(matrix):
 
     return (q,r)
 
-def vecze(arr, barr):
-    Q = gramsch(arr)
-    qt = np.transpose(Q[0])
-    z = np.dot(qt, barr)
+def calcula_ze(Q, b):
+    z = np.dot(np.transpose(Q), b)
     return z
+
+def resolve_sistema(R, z):
+    ans = z.copy()
+
+    for i in range(len(z)-1, -1, -1):
+        ans[i] = ans[i] / R[i,i]
+        for s in range(i-1, -1, -1):
+            ans[s] -= R[s,i] * ans[i]
+
+    return ans
+
+def residuo(A, x, b):
+    d = b - A.dot(x)
+    return np.linalg.norm(d)
 
 def main():
     a = np.array([[1,2,-1,3], [2,3,9,16], [-2,0,4,-1], [2,11,5,3], [0,2,-1,4], [1,-3,1,7]])
-    b = gramsch(a)
-    c = np.dot(np.transpose(b[0]), b[0])
-    print(c)
-main()    
+    b = np.array([1,0,1,0,-1,1])
+    
+    Q, R = gramsch(a)
+    QR = Q.dot(R)
+    z = calcula_ze(Q,b)
+    ans = resolve_sistema(R,z)
+    res = residuo(a, ans, b)
+
+    print('Matrix A:')
+    print(a, '\n')
+    print('Matrix b:')
+    print(b, '\n')
+    print('Fatoração A = QR:')
+    print('Matrix Q:\n')
+    print(Q, '\n')
+    print('Matrix R:\n')
+    print(R, '\n')
+    print('Matrix QR sendo um aprox de A: \n')
+    print(QR, '\n')
+    print('Matrix z = Q.transpose * b:\n')
+    print(z, '\n')
+    print('Solução aproximada do sistema linear Ax = b:\n ')
+    print(ans, '\n')
+    print('Resíduo da solução: \n')
+    print(res, '\n')
+
+main()
